b390c222a2c4bce22d55f46564c3a9201504ce47
lux <ssbekmuratov@gmail.com>
2023-04-04 22:38:49 +0600

    first commit

---
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.dice
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
+class MainActivity: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar!!.setTitle("Dice Roll")
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FF03DAC5")))
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+        rollButton.setOnClickListener{
+            diceRole()
+        }
+    }
+
+    private fun diceRole() {
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val randomInt = Random.nextInt(6)+1
+        val result = when (randomInt){
+            6 -> R.drawable.dice_6
+            5 -> R.drawable.dice_5
+            4 -> R.drawable.dice_4
+            3 -> R.drawable.dice_3
+            2 -> R.drawable.dice_2
+            else -> R.drawable.dice_1
+        }
+        diceImage.setImageResource(result)
+    }
+
+}
